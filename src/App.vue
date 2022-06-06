54a38404e18a4c4af1c4771cb649fea5d5322b0f
Cobollatin <Cobollatin@hotmail.com>
2022-06-06 08:00:41 -0500

    Setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
